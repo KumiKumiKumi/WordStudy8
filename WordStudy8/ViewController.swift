0e915db81909204285946730a1262a07ecb07675
KumiKumiKUmi <g12906hk+git@gmail.com>
2015-05-05 15:38:32 +0900

    [add]言語選択機能の追加
    言語選択ボタンから、言語の選択が可能
    言語は４カ国語
    言語の選択はピッカービューを用いた

--- WordStudy8/ViewController.swift
+++ WordStudy8/ViewController.swift
@@ -7,31 +7,37 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource , UIToolbarDelegate {
     var labelQ: UILabel = UILabel(frame: CGRectZero)
-    var labelA: UILabel = UILabel(frame: CGRectZero)
+    var labelA: UILabel = UILabel(frame: CGRectZero);
     let buttonAns = UIButton()
     let buttonNext = UIButton()
     let buttonSelectLang = UIButton()
     
     let wordJ : [String] = ["りんご", "みかん", "ぶどう", "メロン"]
-    let wordE : [String] = ["apple", "orange", "grape", "melon"]
+    var myValues : NSArray = ["英語","ドイツ語", "フランス語", "イタリア語"]
     var chooseLang: Int = 0
+    let labelLang: UILabel = UILabel(frame: CGRectMake(0,0,200,30))
+    
     var randInt: Int = 0
     var myUIPicker: UIPickerView = UIPickerView()
     var myToolBar: UIToolbar!
     var textShowLang: UITextField!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        setAnsButton()
-    setNextButton()
-        showQuestion()
         
+        setLangButton() //言語選択ボタン表示
+        setUIPicherView() //ピッカービューの設定
+        setLangLabel() //言語ラベルを表示
+        
+        setAnsButton()
+        setNextButton()
+        
+        showQuestion()
     }
+    
     
     func onClickShowAnsButton(sender: UIButton){ //「答えを表示」ボタンが押された時の処理
         showAnswer()
@@ -53,12 +59,24 @@ class ViewController: UIViewController {
     
     func showQuestion(){
         makeQuestion()
-        labelQ.text = wordE[randInt]
+        labelQ.text = String(setLang()[randInt])
         labelQ.sizeToFit()
         labelQ.center = CGPointMake(80, 150) //表示位置
         self.view.addSubview(labelQ)
         self.buttonNext.hidden = true //buttonNextを隠す
         self.buttonAns.hidden = false //buttonAnsを表示
+    }
+    
+    func setLang() -> [String]{
+        let wordE : [String] = ["apple", "orange", "grape", "melon"]
+        let wordG : [String] = ["Apfeu", "Apfelsine", "Weintraube", "Melon"]
+        let wordF : [String] = ["Pomme", "Mandarine orange", "Raisin", "Melon"]
+        let wordI : [String] = ["Apple", "Mandarino arancione", "Mandarino arancino", "Melon"]
+        
+        if chooseLang==0  { return wordE }
+        else if chooseLang==1  { return wordG }
+        else if chooseLang==2  { return wordF }
+        else  { return wordI }
     }
     
     func hideQuestion(){
@@ -67,6 +85,7 @@ class ViewController: UIViewController {
         labelQ.center = CGPointMake(80, 150) //表示位置
         self.view.addSubview(labelQ)
     }
+    
     
     func showAnswer(){
         labelA.text = String(wordJ[randInt])
@@ -97,6 +116,7 @@ class ViewController: UIViewController {
         self.buttonAns.hidden = true //buttonAnsを隠す
     }
     
+    
     func setNextButton(){
         buttonNext.setTitle("次の問題", forState: .Normal) //表示されるテキスト
         buttonNext.setTitleColor(UIColor.blueColor(), forState: .Normal) //テキストの色
@@ -110,6 +130,92 @@ class ViewController: UIViewController {
         self.buttonNext.hidden = true //buttonNextを隠す
     }
     
+    /********** 言語選択ボタンの制御 ************/
+    func setLangButton(){
+        buttonSelectLang.setTitle("言語の選択", forState: .Normal) //表示されるテキスト
+        buttonSelectLang.setTitleColor(UIColor.brownColor(), forState: .Normal) //テキストの色
+        buttonSelectLang.frame = CGRectMake(0, 0, 320, 30) //サイズ
+        buttonSelectLang.tag = 1 //タグ番号
+        buttonSelectLang.layer.position = CGPoint(x: self.view.frame.width/2, y:270) //配置場所
+        buttonSelectLang.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.6) //背景色
+        buttonSelectLang.layer.cornerRadius = 0 //角丸
+        buttonSelectLang.addTarget(self, action: "onClickLangButton:", forControlEvents:.TouchUpInside) //タップした時のメソッド
+        self.view.addSubview(buttonSelectLang)
+    }
+    
+    func onClickLangButton(sender: UIButton){  //言語選択ボタンのイベント
+        textShowLang.becomeFirstResponder() //言語のテキストフィールドにフォーカスを当てる
+    }
+    
+    func setLangLabel(){ //言語を表示
+        labelLang.layer.masksToBounds = true
+        labelLang.layer.cornerRadius = 10.0
+        labelLang.text = myValues[0]as? String
+        labelLang.textColor = UIColor.orangeColor()
+        labelLang.textAlignment = NSTextAlignment.Center
+        labelLang.layer.position = CGPoint(x: self.view.bounds.width/2,y: 110) //配置設定
+        self.view.addSubview(labelLang)
+    }
+    
+    func changeLangLabel(){
+        labelLang.text = myValues[chooseLang]as? String
+    }
+    
+    
+    
+    /********** ピッカービューの指定 **********/
+    func setUIPicherView(){
+        
+        //TextField作成
+        textShowLang = UITextField(frame: CGRectMake(130, 300, 0, 0)) //(x座標, y座標, width, height)
+        textShowLang.placeholder = myValues[0] as? String;
+        textShowLang.text = ""
+        self.view.addSubview(textShowLang)
+        
+        //PickerView作成
+        myUIPicker.showsSelectionIndicator = true
+        myUIPicker.delegate = self // Delegateを設定する.
+        
+        myUIPicker = UIPickerView()
+        myUIPicker.showsSelectionIndicator = true
+        myUIPicker.delegate = self
+        
+        
+        //ToolBar作成
+        myToolBar = UIToolbar(frame: CGRectMake(0, self.view.frame.size.height/6, self.view.frame.size.width, 40.0))
+        myToolBar.layer.position = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height-20.0)
+        myToolBar.backgroundColor = UIColor.blackColor()
+        myToolBar.barStyle = UIBarStyle.Black
+        myToolBar.tintColor = UIColor.whiteColor()
+        
+        //ToolBarを閉じるボタンを追加
+        let myToolBarButton = UIBarButtonItem(title: "Close", style: .Bordered, target: self, action: "onClick:")
+        myToolBarButton.tag = 1
+        myToolBar.items = [myToolBarButton]
+        
+        //TextFieldをpickerViewとToolVerに関連づけ
+        textShowLang.inputView = myUIPicker
+        textShowLang.inputAccessoryView = myToolBar
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {  //表示例
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int { //表示個数
+        return myValues.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! { //表示内容
+        return myValues[row] as? String;
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) { //選択時
+        chooseLang = row
+        changeLangLabel()
+        showQuestion()
+    }
+    
     func onClick(sender: UIBarButtonItem) {
         textShowLang.resignFirstResponder()
     }
@@ -118,7 +224,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-

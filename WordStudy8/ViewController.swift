1a24eb92c1f8443ccbab32c4e1544473ed4c51a1
KumiKumiKUmi <g12906hk+git@gmail.com>
2015-05-05 15:34:20 +0900

    英単語練習アプリケーション
    １、英単語が一語表示される
    ２、「答えを見る」ボタンを押すと、日本語の答えが表れ、ボタンが「次の問題」ボタンに変わる
    ３、「次の問題」ボタンを押すと、新たな英単語が現れる→１に戻る
    ※英単語は４語登録されている
    ※問題はランダムに表示される
    ※同じ問題が２度連続で表れることはない

--- WordStudy8/ViewController.swift
+++ WordStudy8/ViewController.swift
@@ -9,12 +9,111 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var labelQ: UILabel = UILabel(frame: CGRectZero)
+    var labelA: UILabel = UILabel(frame: CGRectZero)
+    let buttonAns = UIButton()
+    let buttonNext = UIButton()
+    let buttonSelectLang = UIButton()
+    
+    let wordJ : [String] = ["りんご", "みかん", "ぶどう", "メロン"]
+    let wordE : [String] = ["apple", "orange", "grape", "melon"]
+    var chooseLang: Int = 0
+    var randInt: Int = 0
+    var myUIPicker: UIPickerView = UIPickerView()
+    var myToolBar: UIToolbar!
+    var textShowLang: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setAnsButton()
+    setNextButton()
+        showQuestion()
+        
     }
-
+    
+    func onClickShowAnsButton(sender: UIButton){ //「答えを表示」ボタンが押された時の処理
+        showAnswer()
+    }
+    
+    func onClickNextButton(sender: UIButton){ //「次の問題」ボタンが押された時の処理
+        hideQuestion()
+        hideAnswer()
+        showQuestion()
+    }
+    
+    func makeQuestion(){
+        var r = Int(arc4random() % 4)
+        while randInt == r {
+            r = Int(arc4random() % 4)
+        }
+        randInt = r
+    }
+    
+    func showQuestion(){
+        makeQuestion()
+        labelQ.text = wordE[randInt]
+        labelQ.sizeToFit()
+        labelQ.center = CGPointMake(80, 150) //表示位置
+        self.view.addSubview(labelQ)
+        self.buttonNext.hidden = true //buttonNextを隠す
+        self.buttonAns.hidden = false //buttonAnsを表示
+    }
+    
+    func hideQuestion(){
+        labelQ.text = ""
+        labelQ.sizeToFit()
+        labelQ.center = CGPointMake(80, 150) //表示位置
+        self.view.addSubview(labelQ)
+    }
+    
+    func showAnswer(){
+        labelA.text = String(wordJ[randInt])
+        labelA.sizeToFit()
+        labelA.center = CGPointMake(240, 150)
+        self.view.addSubview(labelA)
+        self.buttonAns.hidden = true //buttonAnsを隠す
+        self.buttonNext.hidden = false //buttonNextを表示
+    }
+    
+    func hideAnswer(){
+        labelA.text = ""
+        labelA.sizeToFit()
+        labelA.center = CGPointMake(240, 150) //表示位置
+        self.view.addSubview(labelA)
+    }
+    
+    func setAnsButton(){
+        buttonAns.setTitle("答えを表示", forState: .Normal) //表示されるテキスト
+        buttonAns.setTitleColor(UIColor.redColor(), forState: .Normal) //テキストの色
+        buttonAns.frame = CGRectMake(0, 0, 100, 30) //サイズ
+        buttonAns.tag = 1 //タグ番号
+        buttonAns.layer.position = CGPoint(x: self.view.frame.width/2, y:200) //配置場所
+        buttonAns.backgroundColor = UIColor(red: 0.7, green: 0.2, blue: 0.2, alpha: 0.2) //背景色
+        buttonAns.layer.cornerRadius = 10 //角丸
+        buttonAns.addTarget(self, action: "onClickShowAnsButton:", forControlEvents:.TouchUpInside) //タップした時のメソッド
+        self.view.addSubview(buttonAns)
+        self.buttonAns.hidden = true //buttonAnsを隠す
+    }
+    
+    func setNextButton(){
+        buttonNext.setTitle("次の問題", forState: .Normal) //表示されるテキスト
+        buttonNext.setTitleColor(UIColor.blueColor(), forState: .Normal) //テキストの色
+        buttonNext.frame = CGRectMake(0, 0, 100, 30) //サイズ
+        buttonNext.tag = 1 //タグ番号
+        buttonNext.layer.position = CGPoint(x: self.view.frame.width/2, y:200) //配置場所
+        buttonNext.backgroundColor = UIColor(red: 0.1, green: 0.2, blue: 0.8, alpha: 0.2) //背景色
+        buttonNext.layer.cornerRadius = 10 //角丸
+        buttonNext.addTarget(self, action: "onClickNextButton:", forControlEvents:.TouchUpInside) //タップした時のメソッド
+        self.view.addSubview(buttonNext)
+        self.buttonNext.hidden = true //buttonNextを隠す
+    }
+    
+    func onClick(sender: UIBarButtonItem) {
+        textShowLang.resignFirstResponder()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

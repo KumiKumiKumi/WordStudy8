ea474b0f9b8b3d1bc0e0e759c1cf5a8cc2d5cd63
KumiKumiKUmi <g12906hk+git@gmail.com>
2015-05-05 15:04:33 +0900

    Initial Commit

---
+++ WordStudy8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordStudy8
+//
+//  Created by KumiHamazaki on 2015/05/05.
+//  Copyright (c) 2015年 KumiHamazaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
